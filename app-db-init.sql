636562e771f2859285c455854187dbc4115aa2b2
olivier-portal <olivierportal09@gmail.com>
2026-09-17 14:41:30 +0200

    refactor In order to avoid miscomprehension for dev password has been changed to password_hash

--- app-db-init.sql
+++ app-db-init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   username varchar UNIQUE NOT NULL,
   email varchar UNIQUE NOT NULL,
-  password varchar NOT NULL,
+  password_hash varchar NOT NULL,
   public_key varchar NOT NULL,
   created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
